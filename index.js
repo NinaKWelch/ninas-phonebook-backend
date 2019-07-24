8e0cb9df11a76008ddfacab0c20fcbe2a22cda0d
NinaKWelch <ninakwelch@gmail.com>
2019-07-24 22:16:30 +0100

    tehtava 3.8

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser')
 var morgan  = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
 
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+   
 let persons = [
     {
         name: 'Arto Hellas',
