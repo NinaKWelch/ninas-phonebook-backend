3c083c418ca045987e6dadfdf01ef4132417a9c5
NinaKWelch <ninakwelch@gmail.com>
2019-07-26 21:11:24 +0100

    tehtava 3.12 final

--- mongo.js
+++ mongo.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length < 5 ) {
-  console.log('Give your password, and persons name and number as arguments, in that order')
-  process.exit(1)
-}
-
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
@@ -21,12 +16,30 @@ const personSchema = new mongoose.Schema({
   
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    name: name,
-    number: number
-})
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    
+    person
+        .save()
+        .then(response => {
+            console.log(`Added ${person.name} number ${person.number} to phonebook`);
+            mongoose.connection.close();
+    })
+} else if (process.argv.length === 3) {
+    console.log('Phonebook:')
 
-person.save().then(response => {
-  console.log(`Added ${person.name} number ${person.number} to phonebook`);
-  mongoose.connection.close();
-})
+    Person
+        .find({})
+        .then(result => {
+            result.forEach(person => {
+            console.log(person.name + ' ' + person.number)
+            })
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Give your password, and persons name and number as arguments, in that order')
+    process.exit(1)
+}
