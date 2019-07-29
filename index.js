a84b0bddacbf91db2f3d3c4b23a569fb9406b0c4
NinaKWelch <ninakwelch@gmail.com>
2019-07-29 21:53:56 +0100

    tehtava 3.19

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     
     const person = new Person({
@@ -91,9 +91,12 @@ app.post('/api/persons', (request, response) => {
     }
     */
 
-    person.save().then(savedPerson => { // response sent only when operation succeeds 
-        response.json(savedPerson.toJSON()) // sent data is formatted
-    })
+    person.save()
+        .then(savedPerson => savedPerson.toJSON()) // data is formatted
+        .then(savedAndFormattedPerson => {
+            response.json(savedAndFormattedPerson)
+        }) 
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -131,6 +134,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
